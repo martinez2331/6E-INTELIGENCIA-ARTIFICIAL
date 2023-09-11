d2f6c25d9156357da8b926851436bc4142a6228e
martinez2331 <brunoaugustopreciado@gmail.com>
2023-09-10 20:37:26 -0600

    Update P1_CAPITULO2_VARIABLES.py

--- P1_CAPITULO2_VARIABLES.py
+++ P1_CAPITULO2_VARIABLES.py
@@ -1,4 +1,4 @@
-mensaje1 = " kayne lover "
-mensaje = 5
-print(mensaje1,mensaje+3)
+mensaje1 = " kayne lover "#mensaje a imprimir 
+mensaje = 5#declarando numero a sumar con su identificador 
+print(mensaje1,mensaje+3)#amanmos a mensaje 1  sumamos mensaje +3
 
