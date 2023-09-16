97a688bb32fec0e9d5c6a5528f0a2789177ce305
martinez2331 <brunoaugustopreciado@gmail.com>
2023-09-15 18:19:36 -0600

    P2-STRINGS-
    CAPITULO 3 STRINGS

--- P2_CAPITULO3_strings.py
+++ P2_CAPITULO3_strings.py
@@ -1,5 +1,12 @@
+###############-AREA DE PRUEBA-###################
+#m1 y  m2  = 'para poner comillas dentro, es necesario poner comillas "simples" desde afuera, y viceversa'
+#print(mensaje)
+###############-AREA DE PRUEBA-###################
 m1 = "esta frase 'prueba'"#prueba con 1 comilla
 m2 = 'esta frase "prueba"'#prueba con 2 comillas
 
 print(m1)# imprime mensaje de m1
 print(m2)#imprime mensaje de m2
+
+frace = '"print()" se utiliza para imprimir valores en la consola'
+print(frase) #imprimimos la frase
