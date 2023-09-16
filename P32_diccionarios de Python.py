28766c547cb46a10336d5e065cd31e7679558ba0
martinez2331 <brunoaugustopreciado@gmail.com>
2023-09-16 01:34:13 -0600

    P32-diccionarios de Python
    USO DE DIRECCIONES

---
+++ P32_diccionarios de Python.py
@@ -0,0 +1,13 @@
+teclado1 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'HyperX Alloy FPS Pro',
+	'Precio': '89,99'
+}
+
+teclado2 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'Corsair K55 RGB',
+	'Precio': '59,99'
+}
+
+print(teclado1)
