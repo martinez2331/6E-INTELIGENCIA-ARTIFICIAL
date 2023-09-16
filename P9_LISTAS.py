e09302bc0ce99eb0e73c72999172139d53bf2976
martinez2331 <brunoaugustopreciado@gmail.com>
2023-09-15 22:41:35 -0600

    P9-LISTAS
    USAR LISTAS PARA QUE SE IMPRIMA UN DATO EN ESPESIFICO

---
+++ P9_LISTAS.py
@@ -0,0 +1,10 @@
+#Crea una lista que contenga los siguientes valores en las posiciones indicadas.
+#"uno" en la posición 4.
+#"dos" en la posición 1.
+#"tres" en la posición 0.
+#"cuatro" en la posición 3.
+#"cinco" en la posición 2.
+
+lista = ["tres","dos","cinco","cuatro","uno"] #creamos lista con las posiciones asignadas
+print(lista) #imprimimos toda la lista
+print(lista[3])#imprimimos el primer dato de la lista
