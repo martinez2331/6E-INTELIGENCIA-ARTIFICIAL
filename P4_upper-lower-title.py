d3b849f655fb906ffa28ef828c22310c9bc97498
martinez2331 <brunoaugustopreciado@gmail.com>
2023-09-15 19:52:01 -0600

    P4-upper(), lower() y title
    CAPITULO 5 USO DE upper(), lower() y title

---
+++ P4_upper-lower-title.py
@@ -0,0 +1,14 @@
+###########metodo upper(), lower() y title()#################
+#.title() ha transformado la primera letra de cada palabra en mayúscula.
+#.lower() que hace exactamente lo contrario, lo convierte todo en minúsculas.
+#.upper() Con este método conseguiremos convertir todos y absolutamente todos los caracteres de un string en mayúsculas
+
+nombre ="pura doble pp".title()#las primeras letras en mayusculas
+print(nombre)
+
+nombre1 ="UNA GORRA DE PEZCADO".lower()#todas las letras en minusculas
+print(nombre1)
+
+nombre2 ="ESTA FRASE ESTA EN MAYUSCULAS ".upper()#todas las letras en minusculas 
+print(nombre2)
+
