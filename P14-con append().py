e482a13e05601e3ce2972e21ebe27f191826f059
martinez2331 <brunoaugustopreciado@gmail.com>
2023-09-15 23:29:08 -0600

    P14-con append()
    LE METEMOS DATOS CON APPEEND()

---
+++ P14-con append().py
@@ -0,0 +1,10 @@
+#Añade los colores 'fuxia' y 'celeste' a la lista.
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+#creamos la lista colores
+
+colores.append('GPI') #añadimos color con comillas simples
+colores.append("CHEVE")#añadimos color con comillas dobles
+
+print(colores) #mostramos lalista con los elementos agregados
+
