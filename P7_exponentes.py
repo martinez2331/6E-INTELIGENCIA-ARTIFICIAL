60527120200891b52ed6bafb474a88400791a253
martinez2331 <brunoaugustopreciado@gmail.com>
2023-09-15 22:30:27 -0600

    P7-EXPONENTES
    NUMEROS ELEVADOS

---
+++ P7_exponentes.py
@@ -0,0 +1,11 @@
+################calcular exponentes en Python#################
+#ES CALCULAR UN NUMERO ELEVADO A SU NUMERO COMO 2 ELEVADO A LA 2 
+
+elevado = 5 ** 5 
+print(elevado)
+
+elevado1 = 15 ** 3 
+print(elevado1)
+
+nov = 2*2*2*2*2*2*2*2*2*2
+print(nov)
