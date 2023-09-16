0c163404fec94da275884c4d28e1d5a53a7d963b
martinez2331 <brunoaugustopreciado@gmail.com>
2023-09-16 00:35:48 -0600

    P16_Ordenar_sort
    C17

---
+++ P16_Ordenar_sort.py
@@ -0,0 +1,8 @@
+#Ordena la siguiente lista en orden alfabético descendente (de la letra 'z' a la 'a').
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marron',
+          'lila', 'negro', 'rosa', 'blanco', 'naranja']  #creamos lista desordenada
+
+colores.sort(reverse=True) #ordenamos con reverse para invertir el olden alfabetico
+
+print(colores) #mostramos lista ya ordenada 
