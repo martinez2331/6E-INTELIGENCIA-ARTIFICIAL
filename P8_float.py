5fb68ae315aa9f46a4fe8905a1283e3c767de279
martinez2331 <brunoaugustopreciado@gmail.com>
2023-09-15 22:37:01 -0600

    P8-float_y_round
    uso de numeros decimales

---
+++ P8_float.py
@@ -0,0 +1,9 @@
+
+num = 17.567383292929200234 # declaramos la variable de tipo float
+
+print(round(num,5)) #utilizamos el round para decir cuantos decimales muestre en el print
+###### a qui se imprime todos los decimales que sean
+sum = 23.655
+sum1 = 2.6754690
+hg = sum + sum1 
+print(hg)
