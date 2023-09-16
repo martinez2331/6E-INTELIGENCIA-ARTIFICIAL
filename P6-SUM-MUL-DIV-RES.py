90f1680f4114e8cce7b68213c7d1420cf92bcaaf
martinez2331 <brunoaugustopreciado@gmail.com>
2023-09-15 22:09:34 -0600

    P6-MULTI-SUMA-RES-DIVI
    OPERACIONES COMPLEJAS DE CALCULADORA

---
+++ P6-SUM-MUL-DIV-RES.py
@@ -0,0 +1,18 @@
+#######################################La suma, resta, multiplicación y división en Python
+#suma 
+#resta
+#multiplicacion
+#diicion
+#op varias operaciones seguidas 
+suma = 23 + 4 + 21
+resta = 100 - 45 - 2 
+multi = 545*3
+divi = 16/2 
+op = 10+2-4/2
+
+print(suma,resta,multi,divi,op)
+
+
+
+
+
