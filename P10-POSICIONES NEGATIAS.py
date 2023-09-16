44b78c314e2df0e1f2e9ae9e036fcdac27183ee7
martinez2331 <brunoaugustopreciado@gmail.com>
2023-09-15 23:00:18 -0600

    10Listasposnegativas
    se agarra la lista en negatio

---
+++ P10-POSICIONES NEGATIAS.py
@@ -0,0 +1,11 @@
+#Utiliza las posiciones negativas para acceder e imprimir algunos de los colores de esta lista.Acceder a:
+#   'naranja', 'amarillo', 'lila', 'blanco' y 'rojo'.
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+#creamos la lista
+
+print(colores[-1]) #imprimos el ultimo color/elemento de la lista, color naranja
+print(colores[3]) #imprimos color amarillo
+print(colores[-5]) #imprimos color lila
+print(colores[-2]) #imprimos el penultimo color/elemento de la lista, color blanco
+print(colores[0]) #imprimimos el color rojo
