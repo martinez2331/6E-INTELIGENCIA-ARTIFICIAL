a7d9740b0ddad46c34df62cd8cdaff976bbc51dd
martinez2331 <brunoaugustopreciado@gmail.com>
2023-09-15 18:58:29 -0600

    P3-CONECTAR
    CAPITULO 4 - COMO CONECTAR

---
+++ P3_CONECTAR.py
@@ -0,0 +1,29 @@
+###############-AREA DE PRUEBA-###################
+#palabra_1 = 'ing.'
+#palabra_2 = 'mecatronica'
+#frase1 = palabra_1 + " " + palabra_2 + "."
+#print(frase1)
+###############-AREA DE PRUEBA-###################
+palabra_1 = 'ing.'
+palabra_2 = 'mecatronica'
+frase1 = palabra_1 + " " + palabra_2 + "."
+print(frase1)
+
+num1 = '7'
+num2 = '8'
+num4 = 1
+num5 = 2
+suma = num1 + num2
+print(suma)
+numero3 = num5 + num4
+print(numero3)
+
+nova = "adios"+"baybay"
+print(nova)
+
+nom1 = "bruno "#declaro nombre1
+nom2 = "augusto "#declaro nombre2
+ape1 = "precido"#declaro mi primer apellido
+ape2 = "martinez"#declaro mi segundo apellido
+nomc = nom1+nom2+ape1+ape2#concateno mi nombre completo
+print(nomc)#e imprimo
