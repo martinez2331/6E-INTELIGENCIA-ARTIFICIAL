891c9fd49e7f085131a71b8f2838fca2396445a7
martinez2331 <brunoaugustopreciado@gmail.com>
2023-09-16 00:49:17 -0600

    P17._elementos_lista_leng
    LISTA  LEENG

---
+++ P17__elementos_lista.py
@@ -0,0 +1,8 @@
+#contar los elementos de una lista
+
+colores = ['rojo', 'azul', 'verde', 'amarillo'] #creamos lista
+
+print(len(colores)) #imprimimos en consola el numero de elementos de la lista sin almacenar el valor
+
+longitud_colores = len(colores) #almacenamos el numero de elementos en una variable
+print(longitud_colores) #mostramos el valor
