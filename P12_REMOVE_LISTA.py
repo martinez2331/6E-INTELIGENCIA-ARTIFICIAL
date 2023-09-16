b9d863671f5c1914c704ad3fb641f1035087c911
martinez2331 <brunoaugustopreciado@gmail.com>
2023-09-15 23:17:42 -0600

    P12_REMOVE_LISTA
    capitulo 12 lo mismo que el 11 solo con remove

---
+++ P12_REMOVE_LISTA.py
@@ -0,0 +1,12 @@
+# Elimina de la siguiente lista los elementos 'amarillo' y 'rojo'.
+# Solo puedes eliminarlos utilizando el método remove().
+
+colores = ['rojo', 'azul', 'verde','gpi','cheve', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+#declaramos lista
+
+colores.remove('negro') #eliminamos el negro
+colores.remove("rojo") #eliminamos el rojo
+colores.remove('gpi') #eliminamos el negro
+
+
+print(colores) #corroboramos que se hayan eliminado los elementos
