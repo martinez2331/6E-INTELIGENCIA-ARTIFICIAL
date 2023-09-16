000c28ee9f24a99e3d534f28843fd0401825472d
martinez2331 <brunoaugustopreciado@gmail.com>
2023-09-16 02:03:16 -0600

    P40_self

---
+++ P40_self.py
@@ -0,0 +1,15 @@
+class Usuario:
+	def __init__(self, nombre, apellidos)
+		self.nombre = nombre
+	self.apellidos = apellidos
+
+	def imprime_datos(self):
+		print('Nombre:', self.nombre, '\nApellidos:', self.apellidos)
+
+usuario001 = Usuario('Enrique', 'Barros Fernández')
+
+usuario002 = Usuario('Javier', 'Gomila Reyes')
+
+usuario002.nombre = 'Jacinto'
+
+usuario002.imprime_datos()
