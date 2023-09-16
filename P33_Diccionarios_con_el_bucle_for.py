7e47bc0b27a5a9529f955d1a7b4c8a7944d71743
martinez2331 <brunoaugustopreciado@gmail.com>
2023-09-16 01:37:57 -0600

    P33_Diccionarios_con_el_bucle_for
    CON BUCLE FORR

---
+++ P33_Diccionarios_con_el_bucle_for.py
@@ -0,0 +1,30 @@
+teclado1 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'HyperX Alloy FPS Pro',
+	'Precio': '89,99'
+}
+
+teclado2 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'Corsair K55 RGB',
+	'Precio': '59,99'
+}
+
+for x in teclado2:
+	print(x)
+	
+
+teclado1 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'HyperX Alloy FPS Pro',
+	'Precio': '89,99'
+}
+
+teclado2 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'Corsair K55 RGB',
+	'Precio': '59,99'
+}
+
+for x in teclado2:
+	print(teclado2[x])
