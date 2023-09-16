2f1f3a3de56d84c106f1180a998362a7b3b3f33c
martinez2331 <brunoaugustopreciado@gmail.com>
2023-09-15 22:23:04 -0600

    P5-TABULADOR-ESPACIO
    C6 TABULADOR  ESPACIO

---
+++ P5_SALTO_DE_LINEA__TABULADOR.py
@@ -0,0 +1,11 @@
+########################hacer saltos de línea y tabulaciones literales en Python######################
+#\n representa una pulsación de la tecla enter 
+#\t representa un salto de linea 
+
+
+curso = "lista de materias de ing. mecatronica:\n\tI.A\n\tVibraciones.\n\tingles.\n\t.control.\n\tplc.\n\tvision\n\teconomia\n\tmicrocontroladores" 
+print(curso)
+
+p1 = "\n\tI-Python.\n\tI -JavaScript.\n\tI -Java.\n\tI -PHP.\n\tI -TypeScript.\n\tI -SQL.\n\tI -COBOL."
+print(p1)
+
