05acad49d7faa63371497658cfac321e299fd30b
martinez2331 <brunoaugustopreciado@gmail.com>
2023-09-16 01:59:13 -0600

    P39_El_método___init__

---
+++ P39_El_método___init__.py
@@ -0,0 +1,11 @@
+class Usuario:
+	def __init__(self, nombre, apellidos)
+		self.nombre = nombre
+	self.apellidos = apellidos
+
+	def imprime_datos(self):
+		print('Nombre:', self.nombre, '\nApellidos:', self.apellidos)
+
+usuario001 = Usuario('Enrique', 'Barros Fernández')
+
+usuario002 = Usuario('Javier', 'Gomila Reyes')
